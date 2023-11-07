6e1df06ff914e0fcd34be87474c3446d5c7c1e43
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-07 18:55:03 -0500

    Spring Core: Setter Injection

---
+++ 02-spring-boot-spring-core/03-setter-injection/src/main/java/com/spring/springcoredemo/rest/CoachController.java
@@ -0,0 +1,22 @@
+package com.spring.springcoredemo.rest;
+
+import com.spring.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CoachController {
+    private Coach coach;
+
+    @Autowired
+    public void setCoach(Coach coach) {
+        this.coach = coach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout() {
+        return coach.getDailyWorkout();
+    }
+
+}
