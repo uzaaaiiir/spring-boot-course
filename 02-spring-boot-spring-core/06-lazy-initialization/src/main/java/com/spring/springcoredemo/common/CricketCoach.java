d795cde6ac5f0796e4c1956cc55e202133d449a5
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-07 20:31:43 -0500

    Spring Core: Lazy Initialization

---
+++ 02-spring-boot-spring-core/06-lazy-initialization/src/main/java/com/spring/springcoredemo/common/CricketCoach.java
@@ -0,0 +1,19 @@
+package com.spring.springcoredemo.common;
+
+import org.springframework.context.annotation.Lazy;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+
+@Component
+@Lazy
+public class CricketCoach implements Coach {
+
+    public CricketCoach() {
+        System.out.printf("In constructor: %s%n", getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice fast bowling for 15 minutes.";
+    }
+}
