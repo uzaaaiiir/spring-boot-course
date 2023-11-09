bab97416cd2064b33b8cc5694ac95bacd9900d33
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-09 16:30:57 -0500

    Hibernate JPA: Automatically create table

---
+++ 03-spring-boot-hibernate-jpa-crud/08-crud-demo-student-create-db-table-from-code/src/main/java/com/spring/demo/CrudDemo/dao/StudentDAOImpl.java
@@ -0,0 +1,69 @@
+package com.spring.demo.CrudDemo.dao;
+
+import com.spring.demo.CrudDemo.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.TypedQuery;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.lang.reflect.Type;
+import java.util.List;
+
+@Repository
+public class StudentDAOImpl implements StudentDAO {
+    private final EntityManager entityManager;
+    @Autowired
+    public StudentDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Student student) {
+        entityManager.persist(student);
+    }
+
+    @Override
+    public Student findById(Integer id) {
+        return entityManager.find(Student.class, id);
+    }
+
+    @Override
+    public List<Student> findAll() {
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student order by lastName asc", Student.class);
+
+        return query.getResultList();
+    }
+
+    @Override
+    public List<Student> findByLastName(String lastName) {
+        // :lastName is a named parameter that will get set later (dynamic)
+        TypedQuery<Student> query = entityManager.createQuery("FROM Student where lastName=:lastName", Student.class);
+
+        // Set query parameter
+        query.setParameter("lastName", lastName);
+
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public void update(Student student) {
+        entityManager.merge(student);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Integer id) {
+        Student student = entityManager.find(Student.class, id);
+
+        entityManager.remove(student);
+    }
+
+    @Override
+    @Transactional
+    public int deleteAll() {
+        return entityManager.createQuery("DELETE FROM Student").executeUpdate();
+    }
+}
