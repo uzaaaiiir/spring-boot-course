0bc2b0133908d62050b54b4fdf50481fbf0b3616
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-09 16:29:46 -0500

    Hibernate JPA: Query findByLastName

---
+++ 03-spring-boot-hibernate-jpa-crud/04-crud-demo-student-query-findByLastName/src/main/java/com/spring/demo/CrudDemo/dao/StudentDAO.java
@@ -0,0 +1,15 @@
+package com.spring.demo.CrudDemo.dao;
+
+import com.spring.demo.CrudDemo.entity.Student;
+
+import java.util.List;
+
+public interface StudentDAO {
+    void save(Student student);
+
+    Student findById(Integer id);
+
+    List<Student> findAll();
+
+    List<Student> findByLastName(String lastName);
+}
