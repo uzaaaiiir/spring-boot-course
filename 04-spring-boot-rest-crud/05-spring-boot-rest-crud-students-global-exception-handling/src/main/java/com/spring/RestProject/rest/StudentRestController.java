e8265b4d74323077297795c1525d429278400461
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-10 09:04:19 -0500

    Spring Boot Rest CRUD: Global exception handling

---
+++ 04-spring-boot-rest-crud/05-spring-boot-rest-crud-students-global-exception-handling/src/main/java/com/spring/RestProject/rest/StudentRestController.java
@@ -0,0 +1,39 @@
+package com.spring.RestProject.rest;
+
+import com.spring.RestProject.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
+
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable(name = "studentId") int studentId) {
+        if (studentId >= students.size() || studentId < 0) {
+            throw new StudentNotFoundException("Student id not found - " + studentId);
+        }
+
+        return students.get(studentId);
+    }
+}
