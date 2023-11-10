965f18b00f2b19f29838b9ac9d2ef5590a86c7c7
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-10 09:03:28 -0500

    Spring Boot Rest CRUD: Path variables

---
+++ 04-spring-boot-rest-crud/03-spring-boot-rest-crud-students-path-variables/src/main/java/com/spring/RestProject/rest/StudentRestController.java
@@ -0,0 +1,36 @@
+package com.spring.RestProject.rest;
+
+import com.spring.RestProject.entity.Student;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    List<Student> students;
+
+    @PostConstruct
+    public void loadData() {
+        students = new ArrayList<>();
+
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Mary", "Smith"));
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable(name = "studentId") int studentId) {
+        return students.get(studentId);
+    }
+}
