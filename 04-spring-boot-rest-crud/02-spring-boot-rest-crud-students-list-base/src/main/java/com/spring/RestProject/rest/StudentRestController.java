1d429eadc1a5744dec13266c532defc95ed22c37
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-10 09:02:58 -0500

    Spring Boot Rest CRUD: DAO findAll

---
+++ 04-spring-boot-rest-crud/02-spring-boot-rest-crud-students-list-base/src/main/java/com/spring/RestProject/rest/StudentRestController.java
@@ -0,0 +1,24 @@
+package com.spring.RestProject.rest;
+
+import com.spring.RestProject.entity.Student;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class StudentRestController {
+    @GetMapping("/students")
+    public List<Student> getStudents() {
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student("Poornima", "Patel"));
+        students.add(new Student("Mario", "Rossi"));
+        students.add(new Student("Mary", "Smith"));
+
+        return students;
+    }
+}
