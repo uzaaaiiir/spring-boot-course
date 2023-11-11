40df419f69ee10d2ac19a9e4377e7fb2128bb047
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-11 16:20:26 -0500

    Spring MVC: Intro to ThymeLeaf templating

---
+++ 06-spring-boot-spring-mvc/01-thyme-leaf-demo-form-data/src/main/java/com/springboot/thymeleafdemo/controller/HelloWorldController.java
@@ -0,0 +1,17 @@
+package com.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/showForm")
+    public String showForm() {
+        return "helloworld-form";
+    }
+
+    @RequestMapping("/processForm")
+    public String processForm() {
+        return "helloworld";
+    }
+}
