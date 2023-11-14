20f5198d32e308a9667284230295fc42c9a5d284
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-13 21:21:24 -0500

    Spring MVC Security: Testing JDBC connection w plain text pw's for security

---
+++ 08-spring-boot-spring-mvc-security/03-spring-boot-spring-mvc-security-plain-text-passwords/src/main/java/com/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -0,0 +1,39 @@
+package com.springboot.demosecurity.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DemoSecurityConfig {
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configure ->
+                        configure
+                                .requestMatchers("/").hasRole("EMPLOYEE")
+                                .requestMatchers("/leaders/**").hasRole("MANAGER")
+                                .requestMatchers("/systems/**").hasRole("ADMIN")
+                                .anyRequest().authenticated())
+                .formLogin(form ->
+                    form.loginPage("/showMyLoginPage")
+                            .loginProcessingUrl("/authenticateTheUser")
+                            .permitAll()
+                )
+                .logout(LogoutConfigurer::permitAll)
+                .exceptionHandling(configure ->
+                        configure.accessDeniedPage("/access-denied"));
+
+        return http.build();
+    }
+}
