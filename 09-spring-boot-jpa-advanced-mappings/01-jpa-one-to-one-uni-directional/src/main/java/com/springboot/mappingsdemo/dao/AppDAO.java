c4d2865f7655b383ee015e9be162ff31481e6bf6
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-14 22:37:39 -0500

    JPA Advanced Mappings: @OneToOne unidirectional mapping

---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-directional/src/main/java/com/springboot/mappingsdemo/dao/AppDAO.java
@@ -0,0 +1,12 @@
+package com.springboot.mappingsdemo.dao;
+
+import com.springboot.mappingsdemo.entity.Instructor;
+import com.springboot.mappingsdemo.entity.InstructorDetail;
+
+public interface AppDAO {
+    void save(Instructor instructor);
+
+    Instructor findInstructorById(int id);
+
+    void deleteInstructorById(int id);
+}
---
+++ 09-spring-boot-jpa-advanced-mappings/01-jpa-one-to-one-uni-directional/src/main/java/com/springboot/mappingsdemo/dao/AppDAOImpl.java
@@ -0,0 +1,36 @@
+package com.springboot.mappingsdemo.dao;
+
+import com.springboot.mappingsdemo.entity.Instructor;
+import jakarta.persistence.EntityManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+public class AppDAOImpl implements AppDAO {
+    private EntityManager entityManager;
+
+    @Autowired
+    public AppDAOImpl(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    @Override
+    @Transactional
+    public void save(Instructor instructor) {
+        entityManager.persist(instructor);
+    }
+
+    @Override
+    public Instructor findInstructorById(int id) {
+        return entityManager.find(Instructor.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void deleteInstructorById(int id) {
+        Instructor instructor = entityManager.find(Instructor.class, id);
+
+        entityManager.remove(instructor);
+    }
+}
