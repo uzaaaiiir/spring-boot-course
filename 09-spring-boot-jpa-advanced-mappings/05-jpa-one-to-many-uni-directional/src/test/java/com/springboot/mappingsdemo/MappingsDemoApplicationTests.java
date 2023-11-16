0260d7f33c0ac2e8c4e419e25a29ca547795e7d4
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-15 21:44:46 -0500

    JPA Advanced Mappings: @OneToMany Uni-directional mapping

---
+++ 09-spring-boot-jpa-advanced-mappings/05-jpa-one-to-many-uni-directional/src/test/java/com/springboot/mappingsdemo/MappingsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.mappingsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
