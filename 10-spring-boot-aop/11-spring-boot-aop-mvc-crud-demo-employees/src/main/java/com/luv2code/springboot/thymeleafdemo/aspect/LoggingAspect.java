37735efb50a467b0b2bad7292bb4f710d47d6dd2
Uzair Mohiuddin <uzair_mohiuddin@hotmail.com>
2023-11-19 10:16:10 -0500

    Spring AOP: Implementing AOP into MVC app

---
+++ 10-spring-boot-aop/11-spring-boot-aop-mvc-crud-demo-employees/src/main/java/com/luv2code/springboot/thymeleafdemo/aspect/LoggingAspect.java
@@ -0,0 +1,54 @@
+package com.luv2code.springboot.thymeleafdemo.aspect;
+
+import org.apache.tomcat.Jar;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import org.springframework.stereotype.Component;
+
+import java.util.logging.Logger;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    private Logger logger = Logger.getLogger(getClass().getName());
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.controller.*.*(..))")
+    private void forControllerPackage() {}
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.service.*.*(..))")
+    private void forServicePackage() {}
+
+    @Pointcut("execution(* com.luv2code.springboot.thymeleafdemo.dao.*.*(..))")
+    private void forDaoPackage() {}
+
+    @Pointcut("forControllerPackage() || forServicePackage() || forDaoPackage()")
+    private void forAppFlow() {}
+
+    @Before("forAppFlow()")
+    public void before(JoinPoint joinPoint) {
+        MethodSignature methodSignature = (MethodSignature) joinPoint.getSignature();
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("=====>In @Before: calling method " + method);
+
+        Object[] args = joinPoint.getArgs();
+
+        for (Object arg : args) {
+            logger.info("====>argument: " + arg);
+        }
+    }
+
+    @AfterReturning(
+            pointcut = "forAppFlow()",
+            returning = "result"
+    )
+    public void afterReturning(JoinPoint joinPoint, Object result) {
+        String method = joinPoint.getSignature().toShortString();
+        logger.info("=====>In @AfterReturning from method " + method);
+
+        logger.info("=====>Result: " + result);
+    }
+}
